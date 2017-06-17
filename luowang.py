d2e1554a2e43ab286200571b5874da4912b93df9
wxrapha <xiehao@xiehaodeMacBook-Pro.local>
2017-06-17 23:16:28 +0800

    更新关键词搜索图片

--- luowang.py
+++ luowang.py
@@ -5,7 +5,9 @@ __date__ = '2017/6/12 下午2:42'
 import requests
 import re
 
-url = 'https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=%E6%9A%B4%E8%B5%B0%E6%BC%AB%E7%94%BB&ct=201326592&ic=0&lm=-1&width=&height=&v=flip'
+word = raw_input("Input key word: ")
+url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word='+word+'&ct=201326592&v=flip'
+result = requests.get(url)
 
 html = requests.get(url).text
 pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
@@ -15,7 +17,7 @@ for each in pic_url:
     try:
         pic = requests.get(each,timeout = 10)
     except requests.exceptions.ConnectionError:
-        print '错误'
+        print '错误:当前图片无法下载'
         continue
     string = 'pictures\\' + str(i) + '.jpg'
     fp = open(string, 'wb')
