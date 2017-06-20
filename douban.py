41f023ec32d6d19eba1958b1297dfb710a86ea66
wxrapha <348698588@qq.com>
2017-06-20 13:30:41 +0800

    Update douban.py

--- douban.py
+++ douban.py
@@ -11,18 +11,18 @@ import re
 
 class DouBan:
     def __init__(self):
-
+        #登陆URL
         self.loginUrl = 'https://www.douban.com/accounts/login'
-
+        #CookieJar对象
         self.cookies = cookielib.CookieJar()
-
+        #表单数据
         self.postdata = urllib.urlencode({
-            'form_email': '15062255019',
-            'form_password': 'xh940723'
+            'form_email': '账号',
+            'form_password': '密码'
         })
-
+        #构建opener
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
-
+    #获取页面
     def getPage(self):
         request = urllib2.Request(
             url=self.loginUrl,
